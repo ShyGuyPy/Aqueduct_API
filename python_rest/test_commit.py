c921d7123516f0aed713c9cb27a76d566a48d0be
ShyGuyPy <lvawter@icprb.org>
2019-05-09 17:47:25 -0400

    postgres db ready to access

---
+++ python_rest/test_commit.py
@@ -0,0 +1,36 @@
+CREATE TABLE test_table7(
+    Id INT PRIMARY KEY NOT NULL,
+    Year INT  NOT NULL,
+    Month INT NOT NULL,
+    Res INT,
+    solids BOOLEAN,
+    NTU BOOLEAN,
+    org_ml BOOLEAN,
+    MPN_100ml BOOLEAN,
+    MPN_100ml__1 BOOLEAN,
+    LosR FLOAT,
+    Silica FLOAT,
+    Ca  FLOAT,
+    Mg FLOAT,
+    Ca_Mg FLOAT,
+    NO3 FLOAT,
+    NO3_USGS FLOAT,
+    Cl2 FLOAT,
+    Na FLOAT,
+    SO4 FLOAT,
+    K FLOAT,
+    pH FLOAT,
+    Alk FLOAT,
+    Hard FLOAT,
+    Nhard FLOAT,
+    C_USGSTemp FLOAT,
+    F_USGS FLOAT,
+    Temp FLOAT,
+    MD_Precip_inch_mon FLOAT,
+    MD_Temp_F FLOAT
+);
+
+COPY test_table FROM 'C:\Users\icprbadmin\Documents\Python_Scripts\python_rest\Reservoir_intake.csv' DELIMITER ';' CSV HEADER;
+C:\Users\icprbadmin\Documents\Python_Scripts\python_rest
+
+COPY test_table7 FROM 'Reservoir_intake_tweaked.csv' DELIMITER ',' CSV HEADER;
