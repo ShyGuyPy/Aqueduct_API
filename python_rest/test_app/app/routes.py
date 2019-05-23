378fee9d972b6b3f620448e37ef9d8def5950009
ShyGuyPy <lvawter@icprb.org>
2019-05-23 15:21:16 -0400

    working on/playing with template structure of ...something different under /test_app/app

--- python_rest/test_app/app/routes.py
+++ python_rest/test_app/app/routes.py
@@ -5,10 +5,43 @@ from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-# def index():
-#     return "Hello, World!"
+def index():
+    user = {'username': 'ShyGuy'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Test-Man'},
+            'body': 'Everything is okay meow!'
+        },
+        {
+            'author': {'username': 'Test-Man'},
+            'body': 'Everything is okay meow!'
+        },
+        {
+            'author': {'username': 'Test-Man'},
+            'body': 'Everything is okay meow!'
+        },
+        {
+            'author': {'username': 'Test-Man'},
+            'body': 'Everything is okay meow!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+#return render_template('index.html', user=user)
+#return render_template('index.html', title='Home',user=user)
 
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
