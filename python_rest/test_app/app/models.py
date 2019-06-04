09e883f7c7b36f1352939fd9351f9f34cb9f1e25
ShyGuyPy <luke.vawter1@gmail.com>
2019-06-04 17:05:55 -0400

    updated requirements for main and fixed typo in test_app/app/templates/models.py

--- python_rest/test_app/app/models.py
+++ python_rest/test_app/app/models.py
@@ -16,5 +16,5 @@ class Post(db.Model):
     timestamp = db.Column(db.Datetime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(selfself):
+    def __repr__(self):
         return '<Post {}>'.format(self.body)
