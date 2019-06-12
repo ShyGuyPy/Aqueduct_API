a72b11c202091bdf1d24daf9d464d4c78b552314
ShyGuyPy <luke.vawter1@gmail.com>
2019-06-12 14:12:17 -0400

    model for Data db added

--- python_rest/test_app/app/models.py
+++ python_rest/test_app/app/models.py
@@ -33,12 +33,36 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-class Test_data(db.Model):
+class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     month = db.Column(db.Integer)
     res = db.Column(db.Integer)
-    solids = db.Column(db.Integer)
+    solids = db.Column(db.Boolean)
+    NTU = db.Column(db.Boolean)
+    org_mal = db.Column(db.Boolean)
+    MPN_100ml = db.Column(db.Boolean)
+    MPN_100ml_1 = db.Column(db.Boolean)
+    LosR = db.Column(db.Integer)
+    Silica = db.Column(db.Integer)
+    Ca = db.Column(db.Integer)
+    Mg = db.Column(db.Integer)
+    Ca_Mg = db.Column(db.Integer)
+    NO3 = db.Column(db.Integer)
+    NO3_USGS = db.Column(db.Integer)
+    Cl2 = db.Column(db.Integer)
+    Na = db.Column(db.Integer)
+    SO4 = db.Column(db.Integer)
+    K = db.Column(db.Integer)
+    pH = db.Column(db.Integer)
+    Alk = db.Column(db.Integer)
+    Hard = db.Column(db.Integer)
+    Nhard = db.Column(db.Integer)
+    C_USGSTemp = db.Column(db.Integer)
+    F_USGS = db.Column(db.Integer)
+    Temp = db.Column(db.Integer)
+    MD_Precip_inch_mon = db.Column(db.Integer)
+    MD_Temp_F = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Data {}>'.format(self.month, self.year, self.id)
